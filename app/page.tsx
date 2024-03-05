304c2459ff9ad0e72f4eb49b43510bf267eef447
senthil-developer <senthildeveloper4@example.com>
2024-03-05 18:25:05 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 import MotionDiv from "@/components/MotionDiv";
 
 export default function Home() {
+  const test = {
+    initial: {
+      x: 1000,
+      opacity: 0,
+    },
+    animate: {
+      x: 1,
+      opacity: 1,
+    },
+    transition: {
+      duration: 2,
+      type: "spring",
+      delay: 1,
+    },
+  };
+
   return (
     <div className="h-[95vh]">
       hello
-      <MotionDiv
-        initial={{ x: 1000 }}
-        animate={{ x: 1 }}
-        transition={{ duration: 0.7, delay: 1 }}
-      >
+      <MotionDiv {...test} className={"bg-red-500"}>
         hello
       </MotionDiv>
     </div>
--- components/MotionDiv.tsx
+++ components/MotionDiv.tsx
@@ -1,39 +1,17 @@
 "use client";
 
-import {
-  AnimationControls,
-  MotionAdvancedProps,
-  Target,
-  TargetAndTransition,
-  Transition$1,
-  VariantLabels,
-  inView,
-  motion,
-} from "framer-motion";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 const MotionDiv = ({
   children,
   className,
-  initial,
-  animate,
-  transition,
   ...props
 }: {
   children: React.ReactNode;
-  className?: "";
-  initial: boolean | Target | VariantLabels;
-  animate: VariantLabels | AnimationControls | TargetAndTransition | undefined;
-  transition?: Transition$1 | undefined;
+  className?: string;
 }) => {
   return (
-    <motion.div
-      className={className}
-      initial={initial}
-      animate={animate}
-      transition={transition}
-      {...props}
-    >
+    <motion.div className={className} {...props}>
       {children}
     </motion.div>
   );
