43b1d5938eb1d04bc106ebeced8c6d392e4dc65c
senthil-developer <senthildeveloper4@example.com>
2024-03-13 21:15:45 +0530

    done

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -5,7 +5,7 @@ import PLaceHolderComponent from "@/components/PLaceHolderComponent";
 import { Suspense } from "react";
 
 import html from "@/public/skill/html.png";
-import react from "@/public/skill/react.png";
+import react from "@/public/skill/reactJs.png";
 import css from "@/public/skill/css.png";
 import js from "@/public/skill/js.png";
 import nextJs from "@/public/skill/nextJs.png";
@@ -29,7 +29,7 @@ const SkillsPage = () => {
     { icon: "/skill/css.png", name: "CSS", placeholder: css },
     { icon: "/skill/js.png", name: "JavaScript", placeholder: js },
     { icon: "/skill/ts.png", name: "TypeScript", placeholder: ts },
-    { icon: "/skill/react.png", name: "ReactJS", placeholder: react },
+    { icon: "/skill/reactJs.png", name: "ReactJS", placeholder: react },
     { icon: "/skill/nextJs.png", name: "NextJs", placeholder: nextJs },
     { icon: "/skill/threeJs.png", name: "ThreeJs", placeholder: threeJs },
     { icon: "/skill/zustand.png", name: "Zustand", placeholder: zustand },
